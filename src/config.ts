454c7aa4e881a0f007f33077e8c4bf85c9a69b20
bielfreire <cf_biel@hotmail.com>
2025-05-23 14:20:53 -0300

    depois descomentar (mudanod rotas para raiwlay )

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   apiUrl: process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:4000'
+    ? 'https://livhe-production.up.railway.app'
     : 'http://127.0.0.1:4000', // Em produção, o backend roda localmente
   isDev: process.env.NODE_ENV === 'development'
-}; 
+};
